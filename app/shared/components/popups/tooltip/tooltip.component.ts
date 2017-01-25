b67f0cac639b2defc9d17b3b3d221471925b6b06
brian treese <brian@socreate.it>
2017-01-25 01:47:04 -0800

    initial commit

---
+++ app/shared/components/popups/tooltip/tooltip.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'saa-tooltip',
+    templateUrl: 'tooltip.component.html',
+    styleUrls: ['tooltip.component.css']
+})
+
+export class ToolTipComponent { }
