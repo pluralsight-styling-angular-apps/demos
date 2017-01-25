8766f1e580cfd8bdb633a0e8d66cd499c2abefd3
brian treese <brian@socreate.it>
2017-01-25 00:19:35 -0800

    initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule }        from '@angular/core';
+import { BrowserModule }   from '@angular/platform-browser';
+import { AppComponent }    from './app.component';
+import { AppNavComponent } from './app-nav.component'
+
+@NgModule({
+  imports:      [ 
+    BrowserModule 
+  ],
+  declarations: [ 
+      AppComponent,
+      AppNavComponent
+  ],
+  bootstrap:    [ AppComponent ]
+})
+
+export class AppModule { }
