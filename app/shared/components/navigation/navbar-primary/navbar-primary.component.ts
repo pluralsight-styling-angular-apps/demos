094915ebe1a5eb7ddd843fd1bea18d86c65e397b
brian treese <brian@socreate.it>
2017-01-25 01:45:45 -0800

    initial commit

---
+++ app/shared/components/navigation/navbar-primary/navbar-primary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'saa-navbar-primary',
+  templateUrl: 'navbar-primary.component.html',
+  styleUrls: ['navbar-primary.component.css']
+})
+
+export class NavBarPrimaryComponent { }
