d8b63ff32b29d2c4cfddb28847ba488d2139108a
brian treese <brian@socreate.it>
2017-01-25 00:20:19 -0800

    initial commit

---
+++ app/app-nav.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'saa-app-nav',
+    template: `
+        <div>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Prerequisites</a>
+                </li>
+                <li>
+                    <a href="#">Modules</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+            </ul>
+        </div>
+    `,
+    styleUrls: ['app-nav.component.css']
+})
+
+export class AppNavComponent {
+}
