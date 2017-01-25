d76b19cded9ba66de4e4e8d53897662b8d62ee9c
brian treese <brian@socreate.it>
2017-01-25 00:32:48 -0800

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'saa-app',
+  encapsulation: ViewEncapsulation.None,
+  template: `
+    <header>
+      <h1>Styling Angular Applications</h1>
+      <em>For Angular Versions 2.0+</em>
+    </header>
+    <div>
+      <saa-app-nav></saa-app-nav>
+    </div>
+  `,
+  styles: [`
+
+    body {
+      color: #666;
+      font-family: arial, sans-serif;
+      margin: 0;
+      padding: 1.5em;
+    }
+
+    h1 {
+      color: #2A9FBC;
+      font-size: 200%;
+      line-height: 1;
+      margin: 0.5em 0 0.25em;
+    }
+
+  `]
+})
+
+export class AppComponent {
+}
